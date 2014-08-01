a40d7c064e670d1a52ee64c324a2feb31e8c4420
Aram Kocharyan <akarmenia@gmail.com>
2014-08-01 12:53:10 +1000

    Removed from server for now due to conflicts.

--- package.js
+++ package.js
@@ -1,4 +1,5 @@
 Package.on_use(function (api) {
   api.export(['define', 'require', 'requirejs'], ['client', 'server']);
-  api.add_files(['require.js'], ['client', 'server']);
+  // TODO(aramk) Conflicts on the server prevent usage for now.
+  api.add_files(['require.js'], ['client']);
 });

84b34858c31479d87e59a6e7fffa218a87b90fb5
Aram Kocharyan <akarmenia@gmail.com>
2015-08-15 12:12:47 +1000

    Re-enabled define.amd by default.

--- meteor.js
+++ meteor.js
@@ -3,11 +3,10 @@ if (Meteor.isServer) {
         define._amd = define.amd;
         delete define.amd;
     };
+    // Useful to prevent node modules on the server from registering as RequireJS modules when they
+    // are imported with NodeJS require() calls.
     requirejs.disableAmd = function() {
         define.amd = define._amd;
         delete define._amd;
     };
-    // Prevent node modules on the server from registering as RequireJS modules when they are imported
-    // with NodeJS require() calls.
-    requirejs.disableAmd();
 }
